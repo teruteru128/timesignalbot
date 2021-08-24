ee105e0c21f1608f519a551c4d9959e65def6c33
Teruteru <teruterubouzu1024@gmail.com>
2021-08-24 22:39:28 +0900

    Update events messages

--- main.py
+++ main.py
@@ -105,7 +105,7 @@ async def on_member_join(member: discord.Member):
 @bot.event
 async def on_member_update(before: discord.Member, after: discord.Member):
     print(
-        f'memberが更新したぜ。: {before.display_name}, {before.roles}, {before.pending}→{after.display_name}, {after.roles}, {after.pending}')
+        f'memberが更新したぜ。: {before.display_name}, {before.status}, {before.activity}, {before.nick}, {before.roles}, {before.roles}, {before.pending}→{after.display_name}, {after.roles}, {after.pending}')
 
 
 @bot.event
