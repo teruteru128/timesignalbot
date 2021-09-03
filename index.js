175f00ed60ec71937dc958d4b12fbb486be952de
Teruteru <teruterubouzu1024@gmail.com>
2021-09-04 03:08:41 +0900

    Fix intents

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const Discord = require('discord.js')
-const client = new Discord.Client()
+const {Discord, Intents } = require('discord.js')
+const client = new Discord.Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] })
 
 client.on('ready', () => {
   console.log('${client.user.tag} でログインしています。')
