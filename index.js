5be1c8d770883d9ca78d6ebee96a9e3cf9c95a3c
Teruteru <teruterubouzu1024@gmail.com>
2022-02-06 14:03:11 +0900

    Update

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 client.on('ready', () => {
-  console.log(`${client.user.tag} でログインしています。`)
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`${client.user.tag} でログインしています。`);
 });
 
 client.on('interactionCreate', async interaction => {
@@ -29,4 +28,4 @@ client.on('guildMemberAvailable', c => {
   console.log(c);
 });
 
-client.login(process.env.DISCORD_TOKEN_V2)
+client.login(process.env.DISCORD_TOKEN_V2);
