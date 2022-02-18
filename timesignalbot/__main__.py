29ce60da87fb830dd44737da87705395022864a1
Teruteru <teruterubouzu1024@gmail.com>
2022-02-18 22:55:07 +0900

    Update

--- timesignalbot/__main__.py
+++ timesignalbot/__main__.py
@@ -34,7 +34,7 @@ def main():
     # ループ処理実行
     loop.start(bot)
     print("timesignalbot is bot instance : {}".format(isinstance(bot, commands.Bot)))
-    print("case_insensitive : {}".format(bot.case_insensitive))
+    print("case_insensitive : {}".format(bot.owner_id))
     # コグ登録
     bot.add_cog(KusoCommands(bot))
     bot.add_cog(MinesweepingCog(bot))
