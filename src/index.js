ea790f3a39ae834b98764f502d42cd81852469ba
Teruteru <teruterubouzu1024@gmail.com>
2022-02-20 20:48:56 +0900

    Restore pg sample code

--- src/index.js
+++ src/index.js
@@ -27,6 +27,31 @@ const client = new Client({
     Intents.FLAGS.GUILD_WEBHOOKS
   ]
 });
+// https://devcenter.heroku.com/ja/articles/getting-started-with-nodejs?singlepage=true#-13
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+new Promise(async (resolve, reject) => {
+  // https://node-postgres.com/
+  const client = await pool.connect();
+  try {
+    const result = await client.query('SELECT $1::text as message', ['Hello world!']);
+    console.log(result.rows[0].message); // Hello world!
+    /* const result = await client.query('SELECT $1::text', ['Hello world!']);
+    console.log(result.rows[0]); // Hello world! */
+    /* const result = await client.query('SELECT $1', ['Hello world!']);
+    console.log(result.rows[0]); // Hello world! */
+    resolve();
+  } catch (error) {
+    reject(error);
+  } finally {
+    client.release();
+  }
+}).catch(err => console.error("pg error : %s", err));
 
 /* 
 const data1 = new SlashCommandBuilder().setName().setDescription()
