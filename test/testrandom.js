bf6c7e67688ce541fa5f42cb918013dc4830d90d
Teruteru <teruterubouzu1024@gmail.com>
2022-02-26 08:16:16 +0900

    Update nextFloat

--- test/testrandom.js
+++ test/testrandom.js
@@ -1,23 +1,26 @@
 
+/**
+ * TODO: mochaでベンチマークをとるにはどうしたらいいか？
+ */
 const assert = require('assert');
 const random = require('../src/random');
 
-it('nextFloat', ()=>{
+it('nextFloat', () => {
     var a = random.nextFloat();
     assert(0 <= a && a < 1);
 });
 
-it('nextInt 1', ()=>{
+it('nextInt 1', () => {
     var a = random.nextInt();
     assert(-2147483648 <= a && a <= 2147483647);
 });
 
-it('nextInt 2', ()=>{
+it('nextInt 2', () => {
     var a = random.nextInt(1023);
     assert(0 <= a && a < 1024);
 });
 
-it('nextInt 3', ()=>{
+it('nextInt 3', () => {
     var a = random.nextInt(1024);
     assert(0 <= a && a < 1024);
 });
