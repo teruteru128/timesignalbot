dd7428b11ea3a1046aedb6db120605c172a879c9
Teruteru <teruterubouzu1024@gmail.com>
2022-02-26 17:50:58 +0900

    Update random test

--- test/testrandom.js
+++ test/testrandom.js
@@ -5,22 +5,33 @@
 const assert = require('assert');
 const random = require('../src/random');
 
-it('nextFloat', () => {
-    var a = random.nextFloat();
-    assert(0 <= a && a < 1, `a is ${a}`);
+describe('nextFloat', () => {
+
+    it('range test', () => {
+        let a = 0;
+        for (let i = 0; i < 16; i++) {
+            a = random.nextFloat();
+            assert(0 <= a && a < 1, `a is ${a}`);
+        }
+    });
+
 });
 
-it('nextInt 1', () => {
-    var a = random.nextInt();
-    assert(-2147483648 <= a && a <= 2147483647, `a is ${a}`);
-});
+describe('nextInt', () => {
 
-it('nextInt 2', () => {
-    var a = random.nextInt(1023);
-    assert(0 <= a && a < 1024, `a is ${a}`);
-});
+    it('no arguments', () => {
+        let a = random.nextInt();
+        assert(-2147483648 <= a && a <= 2147483647, `a is ${a}`);
+    });
 
-it('nextInt 3', () => {
-    var a = random.nextInt(1024);
-    assert(0 <= a && a < 1024, `a is ${a}`);
+    it('bound is not a power of 2', () => {
+        let a = random.nextInt(1023);
+        assert(0 <= a && a < 1023, `a is ${a}`);
+    });
+
+    it('bound is a power of 2', () => {
+        let a = random.nextInt(1024);
+        assert(0 <= a && a < 1024, `a is ${a}`);
+    });
+
 });
