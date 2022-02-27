62d262b847d12f7b47620d6abb77c45856a79d52
Teruteru <teruterubouzu1024@gmail.com>
2022-02-27 05:55:45 +0000

    Add signalbuilder test

--- src/signalbuilder.js
+++ src/signalbuilder.js
@@ -2,20 +2,29 @@
 const random = require('./random');
 
 function buildSignal(now) {
-    let prefix = '真夜中';
-    let date = now.getDate();
-    let month = now.getMonth();
-    let day = now.getDay();
-    // choose prefix
-    if (date == 1) {
-      prefix = (date + 1) + '月';
-    } else if (date == 20 && month == 10) {
-      prefix = '20, november';
-    } else if (day == 1) {
-      prefix = '月曜日';
+  let prefix = '真夜中';
+  let secondPrefix = 'だよ';
+  let date = now.getDate();
+  let month = now.getMonth();
+  let day = now.getDay();
+  // choose prefix
+  if (date == 1) {
+    if (month == 0) {
+      // 毎年1月1日
+      prefix = '新年あけましておめでとう';
+      secondPrefix = '';
+    } else {
+      // 毎年2~12月1日
+      prefix = (month + 1) + '月';
     }
-    // build signal message
-    return prefix + 'だよハルト' + 'オ'.repeat(40 + random.nextInt(60));
+  } else if (date == 20 && month == 10) {
+    prefix = '20, november';
+  } else if (day == 1) {
+    prefix = '月曜日';
+  }
+  // build signal message
+  // オは40以上100(未満ではなく)以下
+  return prefix + secondPrefix + 'ハルト' + 'オ'.repeat(40 + random.nextInt(61));
 }
 
 module.exports.buildSignal = buildSignal;
---
+++ test/testsignalbuilder.js
@@ -0,0 +1,61 @@
+
+const assert = require('assert');
+const signalbuilder = require('../src/signalbuilder');
+
+describe('buildSignal', () => {
+
+  it('January 1st of every month', () => {
+    // 2022年1月1日0時0分0秒
+    let date = new Date(2022, 0, 1, 0, 0, 0);
+    assert.match(signalbuilder.buildSignal(date), /^新年あけましておめでとうハルトオ{40,100}$/);
+  });
+
+  it('1st of every month from February to December', () => {
+    // 2022年2月1日0時0分0秒
+    let date = new Date(2022, 1, 1, 0, 0, 0);
+    assert.match(signalbuilder.buildSignal(date), /^2月だよハルトオ{40,100}$/);
+    // 2022年3月1日0時0分0秒
+    date = new Date(2022, 2, 1, 0, 0, 0);
+    assert.match(signalbuilder.buildSignal(date), /^3月だよハルトオ{40,100}$/);
+    // 2022年4月1日0時0分0秒
+    date = new Date(2022, 3, 1, 0, 0, 0);
+    assert.match(signalbuilder.buildSignal(date), /^4月だよハルトオ{40,100}$/);
+    // 2022年5月1日0時0分0秒
+    date = new Date(2022, 4, 1, 0, 0, 0);
+    assert.match(signalbuilder.buildSignal(date), /^5月だよハルトオ{40,100}$/);
+    // 2022年6月1日0時0分0秒
+    date = new Date(2022, 5, 1, 0, 0, 0);
+    assert.match(signalbuilder.buildSignal(date), /^6月だよハルトオ{40,100}$/);
+    // 2022年7月1日0時0分0秒
+    date = new Date(2022, 6, 1, 0, 0, 0);
+    assert.match(signalbuilder.buildSignal(date), /^7月だよハルトオ{40,100}$/);
+    // 2022年8月1日0時0分0秒
+    date = new Date(2022, 7, 1, 0, 0, 0);
+    assert.match(signalbuilder.buildSignal(date), /^8月だよハルトオ{40,100}$/);
+    // 2022年9月1日0時0分0秒
+    date = new Date(2022, 8, 1, 0, 0, 0);
+    assert.match(signalbuilder.buildSignal(date), /^9月だよハルトオ{40,100}$/);
+    // 2022年10月1日0時0分0秒
+    date = new Date(2022, 9, 1, 0, 0, 0);
+    assert.match(signalbuilder.buildSignal(date), /^10月だよハルトオ{40,100}$/);
+    // 2022年11月1日0時0分0秒
+    date = new Date(2022, 10, 1, 0, 0, 0);
+    assert.match(signalbuilder.buildSignal(date), /^11月だよハルトオ{40,100}$/);
+    // 2022年12月1日0時0分0秒
+    date = new Date(2022, 11, 1, 0, 0, 0);
+    assert.match(signalbuilder.buildSignal(date), /^12月だよハルトオ{40,100}$/);
+  });
+
+  it('20, november', () => {
+    // 2022年1月1日0時0分0秒
+    let date = new Date(2022, 10, 20, 0, 0, 0);
+    assert.match(signalbuilder.buildSignal(date), /^20, novemberだよハルトオ{40,100}$/);
+  });
+
+  it('monday', () => {
+    // 2022年1月3日0時0分0秒
+    let date = new Date(2022, 0, 3, 0, 0, 0);
+    assert.match(signalbuilder.buildSignal(date), /^月曜日だよハルトオ{40,100}$/);
+  });
+
+});
