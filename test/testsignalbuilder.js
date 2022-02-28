a40f7c79f49704a15b07a96d4d2cb0069911fe74
Teruteru <teruterubouzu1024@gmail.com>
2022-02-28 12:02:22 +0000

    Update test cases

--- test/testsignalbuilder.js
+++ test/testsignalbuilder.js
@@ -16,37 +16,37 @@ describe('buildSignal', () => {
   it('1st of every month from February to December', () => {
     // 2022年02月01日00時00分00秒
     let date = new Date(2022, 1, 1, 0, 0, 0);
-    assert.match(buildSignal(date), /^2月だよハルトオ{40,100}$/);
+    assert.match(buildSignal(date), /^2月1日だよハルトオ{40,100}$/);
     // 2022年03月01日00時00分00秒
     date = new Date(2022, 2, 1, 0, 0, 0);
-    assert.match(buildSignal(date), /^3月だよハルトオ{40,100}$/);
+    assert.match(buildSignal(date), /^3月1日だよハルトオ{40,100}$/);
     // 2022年04月01日00時00分00秒
     date = new Date(2022, 3, 1, 0, 0, 0);
-    assert.match(buildSignal(date), /^4月だよハルトオ{40,100}$/);
+    assert.match(buildSignal(date), /^4月1日だよハルトオ{40,100}$/);
     // 2022年05月01日00時00分00秒
     date = new Date(2022, 4, 1, 0, 0, 0);
-    assert.match(buildSignal(date), /^5月だよハルトオ{40,100}$/);
+    assert.match(buildSignal(date), /^5月1日だよハルトオ{40,100}$/);
     // 2022年06月01日00時00分00秒
     date = new Date(2022, 5, 1, 0, 0, 0);
-    assert.match(buildSignal(date), /^6月だよハルトオ{40,100}$/);
+    assert.match(buildSignal(date), /^6月1日だよハルトオ{40,100}$/);
     // 2022年07月01日00時00分00秒
     date = new Date(2022, 6, 1, 0, 0, 0);
-    assert.match(buildSignal(date), /^7月だよハルトオ{40,100}$/);
-    // 2022年08月01日00時00分00秒
+    assert.match(buildSignal(date), /^7月1日だよハルトオ{40,100}$/);
+    // 2022年08月01日00時00分00秒=>月曜日
     date = new Date(2022, 7, 1, 0, 0, 0);
-    assert.match(buildSignal(date), /^8月だよハルトオ{40,100}$/);
+    assert.match(buildSignal(date), /^8月1日月曜日だよハルトオ{40,100}$/);
     // 2022年09月01日00時00分00秒
     date = new Date(2022, 8, 1, 0, 0, 0);
-    assert.match(buildSignal(date), /^9月だよハルトオ{40,100}$/);
+    assert.match(buildSignal(date), /^9月1日だよハルトオ{40,100}$/);
     // 2022年10月01日00時00分00秒
     date = new Date(2022, 9, 1, 0, 0, 0);
-    assert.match(buildSignal(date), /^10月だよハルトオ{40,100}$/);
+    assert.match(buildSignal(date), /^10月1日だよハルトオ{40,100}$/);
     // 2022年11月01日00時00分00秒
     date = new Date(2022, 10, 1, 0, 0, 0);
-    assert.match(buildSignal(date), /^11月だよハルトオ{40,100}$/);
+    assert.match(buildSignal(date), /^11月1日だよハルトオ{40,100}$/);
     // 2022年12月01日00時00分00秒
     date = new Date(2022, 11, 1, 0, 0, 0);
-    assert.match(buildSignal(date), /^12月だよハルトオ{40,100}$/);
+    assert.match(buildSignal(date), /^12月1日だよハルトオ{40,100}$/);
   });
 
   it('20, november', () => {
@@ -64,38 +64,38 @@ describe('buildSignal', () => {
   it('1st of every month from February to December, but it\'s monday', () => {
     // TODO: n月1日が月曜日のとき、「n月だよ」と「月曜日だよ」のどちらを叫ぶべきか？
     // 2027年02月01日は月曜日
-    date = new Date(2027, 1, 1, 0, 0, 0);
-    assert.match(buildSignal(date), /^2月だよハルトオ{40,100}$/);
+    let date = new Date(2027, 1, 1, 0, 0, 0);
+    assert.match(buildSignal(date), /^2月1日月曜日だよハルトオ{40,100}$/);
     // 2027年03月01日は月曜日
     date = new Date(2027, 2, 1, 0, 0, 0);
-    assert.match(buildSignal(date), /^3月だよハルトオ{40,100}$/);
+    assert.match(buildSignal(date), /^3月1日月曜日だよハルトオ{40,100}$/);
     // 2024年04月01日は月曜日
     date = new Date(2024, 3, 1, 0, 0, 0);
-    assert.match(buildSignal(date), /^4月だよハルトオ{40,100}$/);
+    assert.match(buildSignal(date), /^4月1日月曜日だよハルトオ{40,100}$/);
     // 2023年05月01日は月曜日
     date = new Date(2023, 4, 1, 0, 0, 0);
-    assert.match(buildSignal(date), /^5月だよハルトオ{40,100}$/);
+    assert.match(buildSignal(date), /^5月1日月曜日だよハルトオ{40,100}$/);
     // 2026年06月01日は月曜日
     date = new Date(2026, 5, 1, 0, 0, 0);
-    assert.match(buildSignal(date), /^6月だよハルトオ{40,100}$/);
+    assert.match(buildSignal(date), /^6月1日月曜日だよハルトオ{40,100}$/);
     // 2024年07月01日は月曜日
     date = new Date(2024, 6, 1, 0, 0, 0);
-    assert.match(buildSignal(date), /^7月だよハルトオ{40,100}$/);
+    assert.match(buildSignal(date), /^7月1日月曜日だよハルトオ{40,100}$/);
     // 2022年08月01日は月曜日
     date = new Date(2022, 7, 1, 0, 0, 0);
-    assert.match(buildSignal(date), /^8月だよハルトオ{40,100}$/);
+    assert.match(buildSignal(date), /^8月1日月曜日だよハルトオ{40,100}$/);
     // 2025年09月01日は月曜日
     date = new Date(2025, 8, 1, 0, 0, 0);
-    assert.match(buildSignal(date), /^9月だよハルトオ{40,100}$/);
+    assert.match(buildSignal(date), /^9月1日月曜日だよハルトオ{40,100}$/);
     // 2029年10月01日は月曜日
     date = new Date(2029, 9, 1, 0, 0, 0);
-    assert.match(buildSignal(date), /^10月だよハルトオ{40,100}$/);
+    assert.match(buildSignal(date), /^10月1日月曜日だよハルトオ{40,100}$/);
     // 2027年11月01日は月曜日
     date = new Date(2027, 10, 1, 0, 0, 0);
-    assert.match(buildSignal(date), /^11月だよハルトオ{40,100}$/);
+    assert.match(buildSignal(date), /^11月1日月曜日だよハルトオ{40,100}$/);
     // 2025年12月01日は月曜日
     date = new Date(2025, 11, 1, 0, 0, 0);
-    assert.match(buildSignal(date), /^12月だよハルトオ{40,100}$/);
+    assert.match(buildSignal(date), /^12月1日月曜日だよハルトオ{40,100}$/);
   });
 
   it('20, november, but it\'s monday', () => {
