554a19d94fd402974b9cab6c68eddb831b5b998d
Teruteru <teruterubouzu1024@gmail.com>
2022-11-21 00:42:38 +0900

    Update

--- src/index.js
+++ src/index.js
@@ -328,38 +328,40 @@ client.on('messageCreate', msg => {
       let a = null;
       do {
         percentage = random.nextInt(1048576);
-        if (percentage < 70000) {
+        if (percentage < 65000) {
           a = '何がクリスマスじゃあい!';
-        } else if (percentage < 120000) {
+        } else if (percentage < 115000) {
           a = 'https://dic.nicovideo.jp/id/4529483';
-        } else if (percentage < 180000) {
+        } else if (percentage < 170000) {
           a = 'https://www.nicovideo.jp/watch/sm975673';
-        } else if (percentage < 240000) {
+        } else if (percentage < 225000) {
           a = 'https://www.nicovideo.jp/watch/sm7755174';
-        } else if (percentage < 300000) {
+        } else if (percentage < 280000) {
           a = 'https://www.nicovideo.jp/watch/sm12271031';
-        } else if (percentage < 360000) {
+        } else if (percentage < 335000) {
           a = 'https://www.nicovideo.jp/watch/sm12904387';
-        } else if (percentage < 420000) {
+        } else if (percentage < 390000) {
           a = 'https://www.nicovideo.jp/watch/sm17071230';
-        } else if (percentage < 480000) {
+        } else if (percentage < 445000) {
           a = 'https://www.nicovideo.jp/watch/sm26450318';
-        } else if (percentage < 540000) {
+        } else if (percentage < 500000) {
           a = 'https://www.nicovideo.jp/watch/sm31568802';
-        } else if (percentage < 600000) {
+        } else if (percentage < 555000) {
           a = 'https://www.nicovideo.jp/watch/sm34346841';
-        } else if (percentage < 660000) {
+        } else if (percentage < 610000) {
           a = 'https://www.nicovideo.jp/watch/sm39120005';
-        } else if (percentage < 720000) {
+        } else if (percentage < 665000) {
           a = 'https://www.nicovideo.jp/watch/so36581429';
-        } else if (percentage < 790000) {
+        } else if (percentage < 730000) {
           a = 'アンアン\n　　　　　　　ｏ\n　　　　ｏ＿ /）\n　 　　 ／＜＜\n\n12月24日の午後9時から翌25日の午前3時までの6時間は\n1年間で最もセックスをする人の多い「性の6時間」です。\n\n貴方の知り合いや友人ももれなくセックスをしています。\n普段はあどけない顔して世間話してるあの娘もセックスをしています。\n貴方が片想いしているあの綺麗な女性もセックスをしています。\n貴方にもし年頃の娘さんや姉・妹がいて、いま家にいないのでしたら間違いなくセックスしてます。\n貴方と別れたあの娘も貴方がその娘にやってきたことを別の男にやられています。\n貴方の将来の恋人や結婚する相手は、いま違う男のいちもつでヒィヒィ言っています。\n貴男が憧れているあのお兄さんやオジサマ方も今宵はベッドであおおーっ!!と他の男性と一夜を共にしていることでしょう。\n\nすべてを諦めましょう。そして、ともに戦いましょう。';
-        } else if (percentage < 920000) {
+        } else if (percentage < 865000) {
           a = 'やらないか！';
-        } else if (percentage < 960000) {
+        } else if (percentage < 905000) {
           a = 'やめないか！';
+        } else if (percentage < 9600000) {
+          a = '(チキンを)食べないか！';
         } else if (percentage < 1000000) {
-          a = 'ここに建てた病院とSEX!';
+          a = 'ここに建てた病院とSEX！';
         } else {
           continue;
         }
