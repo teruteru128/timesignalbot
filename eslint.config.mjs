b7b68a9694b8f5a071a754cf9be8f80fff355a61
Teruteru <teruterubouzu1024@gmail.com>
2026-03-10 17:25:42 +0900

    Reinit eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,8 @@
+import js from "@eslint/js";
 import globals from "globals";
+import { defineConfig } from "eslint/config";
 
-import path from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-import pluginJs from "@eslint/js";
-
-// mimic CommonJS variables -- not needed if using CommonJS
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({baseDirectory: __dirname, recommendedConfig: pluginJs.configs.recommended});
-
-export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
-  ...compat.extends("airbnb"),
-];
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+]);
