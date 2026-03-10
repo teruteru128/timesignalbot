4f5ebd7d07b711b0eb70a53b02a4b5a05e953838
Teruteru <teruterubouzu1024@gmail.com>
2026-03-10 16:23:37 +0900

    Fix test

--- test/testsignalbuilder.js
+++ test/testsignalbuilder.js
@@ -7,7 +7,10 @@ const { buildSignal } = require('../src/modules/signalbuilder');
 describe('test 1', ()=>{
     it('test 1-1', ()=>{
         const date = new Date(2026, 0, 1, 0, 0, 0);
-        const body = buildSignal(date);
+        const obj = {
+            triggeredAt: date
+        };
+        const body = buildSignal(obj);
         assert(1);
     });
 });
