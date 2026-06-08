547e2ec916aed7dc90be2d4d5ba153726e702672
Teruteru <teruterubouzu1024@gmail.com>
2026-06-08 15:20:19 +0900

    危機感感じるんでしたよね？

--- src/index.js
+++ src/index.js
@@ -262,7 +262,7 @@ client.on(Events.MessageCreate, async (message) => {
       message.stickers.forEach(async (sticker) => {
         // おふろんは爆破する
         if (sticker.name.includes('おふろん')) {
-          await message.reply('牧場鯖閉鎖の危機なのだ〜！');
+          await message.reply('牧場鯖爆破の危機なのだ〜！');
         }
       });
     }
